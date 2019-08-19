908f42a4afe3ab3533d560b346aa73cc66fc8675
mukireus <muhammet.omer@outlook.com>
2019-08-20 01:51:12 +0300

    Bug Fix and Add Icon

--- app/src/main/java/com/mukireus/tictactoe/GameScreenActivity.java
+++ app/src/main/java/com/mukireus/tictactoe/GameScreenActivity.java
@@ -10,115 +10,118 @@ import android.view.View;
 import android.widget.Button;
 
 public class GameScreenActivity extends AppCompatActivity {
-    Button btnOne,btnTwo,btnThree,btnFour,btnFive,btnSix,btnSeven,btnEight,btnNine,btnTry;
-    int sayac = 0;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game_screen);
-        btnOne = findViewById(R.id.btnOneOne);
-        btnTwo = findViewById(R.id.btnOneTwo);
-        btnThree = findViewById(R.id.btnOneThree);
-        btnFour = findViewById(R.id.btnTwoOne);
-        btnFive = findViewById(R.id.btnTwoTwo);
-        btnSix = findViewById(R.id.btnTwoThree);
-        btnSeven = findViewById(R.id.btnThreeOne);
-        btnEight = findViewById(R.id.btnThreeTwo);
-        btnNine = findViewById(R.id.btnThreeThree);
-        btnTry = findViewById(R.id.btnTryAgain);
 
-        btnTry.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = getIntent();
-                finish();
-                startActivity(intent);            }
-        });
+  Button btnOne, btnTwo, btnThree, btnFour, btnFive, btnSix, btnSeven, btnEight, btnNine, btnTry;
+  int sayac = 0;
 
-        btnOne.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("ResourceAsColor")
-            @Override
-            public void onClick(View view) {
-                test(btnOne);
-            }
-        });
-        btnTwo.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("ResourceAsColor")
-            @Override
-            public void onClick(View view) {
-                test(btnTwo);
-            }
-        });
-        btnThree.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("ResourceAsColor")
-            @Override
-            public void onClick(View view) {
-                test(btnThree);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_game_screen);
+    btnOne = findViewById(R.id.btnOneOne);
+    btnTwo = findViewById(R.id.btnOneTwo);
+    btnThree = findViewById(R.id.btnOneThree);
+    btnFour = findViewById(R.id.btnTwoOne);
+    btnFive = findViewById(R.id.btnTwoTwo);
+    btnSix = findViewById(R.id.btnTwoThree);
+    btnSeven = findViewById(R.id.btnThreeOne);
+    btnEight = findViewById(R.id.btnThreeTwo);
+    btnNine = findViewById(R.id.btnThreeThree);
+    btnTry = findViewById(R.id.btnTryAgain);
 
-            }
-        });
-        btnFour.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("ResourceAsColor")
-            @Override
-            public void onClick(View view) {
-                test(btnFour);
+    btnTry.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Intent intent = getIntent();
+        finish();
+        startActivity(intent);
+      }
+    });
 
-            }
-        });
-        btnFive.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("ResourceAsColor")
-            @Override
-            public void onClick(View view) {
-                test(btnFive);
+    btnOne.setOnClickListener(new View.OnClickListener() {
+      @SuppressLint("ResourceAsColor")
+      @Override
+      public void onClick(View view) {
+        Click(btnOne);
+      }
+    });
+    btnTwo.setOnClickListener(new View.OnClickListener() {
+      @SuppressLint("ResourceAsColor")
+      @Override
+      public void onClick(View view) {
+        Click(btnTwo);
+      }
+    });
+    btnThree.setOnClickListener(new View.OnClickListener() {
+      @SuppressLint("ResourceAsColor")
+      @Override
+      public void onClick(View view) {
+        Click(btnThree);
 
-            }
-        });
-        btnSix.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("ResourceAsColor")
-            @Override
-            public void onClick(View view) {
-                test(btnSix);
+      }
+    });
+    btnFour.setOnClickListener(new View.OnClickListener() {
+      @SuppressLint("ResourceAsColor")
+      @Override
+      public void onClick(View view) {
+        Click(btnFour);
 
-            }
-        });
-        btnSeven.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("ResourceAsColor")
-            @Override
-            public void onClick(View view) {
-                test(btnSeven);
-            }
-        });
-        btnEight.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("ResourceAsColor")
-            @Override
-            public void onClick(View view) {
-                test(btnEight);
+      }
+    });
+    btnFive.setOnClickListener(new View.OnClickListener() {
+      @SuppressLint("ResourceAsColor")
+      @Override
+      public void onClick(View view) {
+        Click(btnFive);
 
-            }
-        });
-        btnNine.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("ResourceAsColor")
-            @Override
-            public void onClick(View view) {
-                test(btnNine);
-            }
-        });
+      }
+    });
+    btnSix.setOnClickListener(new View.OnClickListener() {
+      @SuppressLint("ResourceAsColor")
+      @Override
+      public void onClick(View view) {
+        Click(btnSix);
 
+      }
+    });
+    btnSeven.setOnClickListener(new View.OnClickListener() {
+      @SuppressLint("ResourceAsColor")
+      @Override
+      public void onClick(View view) {
+        Click(btnSeven);
+      }
+    });
+    btnEight.setOnClickListener(new View.OnClickListener() {
+      @SuppressLint("ResourceAsColor")
+      @Override
+      public void onClick(View view) {
+        Click(btnEight);
+
+      }
+    });
+    btnNine.setOnClickListener(new View.OnClickListener() {
+      @SuppressLint("ResourceAsColor")
+      @Override
+      public void onClick(View view) {
+        Click(btnNine);
+      }
+    });
+  }
+
+  @SuppressLint("ResourceAsColor")
+  public void Click(Button btn) {
+    if (sayac == 0) {
+      btn.setText("X");
+      btn.setTextSize(70);
+      btn.setTextColor(Color.parseColor("#56E7A5"));
+      btn.setEnabled(false);
+      sayac++;
+    } else {
+      btn.setText("O");
+      btn.setTextSize(70);
+      btn.setTextColor(Color.parseColor("#30D7D7"));
+      btn.setEnabled(false);
+      sayac--;
     }
-    @SuppressLint("ResourceAsColor")
-    public void test(Button btn){
-        if(sayac == 0){
-            btn.setText("X");
-            btn.setTextSize(70);
-            btn.setTextColor(Color.parseColor("#56E7A5"));
-            sayac++;
-        }
-        else{
-            btn.setText("O");
-            btn.setTextSize(70);
-            btn.setTextColor(Color.parseColor("#30D7D7"));
-            sayac--;
-        }
-    }
-
+  }
 }
--- app/src/main/java/com/mukireus/tictactoe/MainActivity.java
+++ app/src/main/java/com/mukireus/tictactoe/MainActivity.java
@@ -1,5 +1,7 @@
 package com.mukireus.tictactoe;
 
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,18 +10,29 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnStart;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        btnStart = findViewById(R.id.btnStart);
-        btnStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, GameScreenActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
+
+  Button btnStart, btnInfo;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    btnStart = findViewById(R.id.btnStart);
+    btnInfo = findViewById(R.id.btnInfo);
+    btnStart.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this, GameScreenActivity.class);
+        startActivity(intent);
+      }
+    });
+    btnInfo.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Toast.makeText(MainActivity.this, "Muhammet Ömer - Github : @mukireus Version 1.0", Toast.LENGTH_LONG).show();
+      }
+    });
+
+
+  }
 }
